7170a01d7c7b89b4308239d15ce9e46962f11553
laura194 <voss.2000@gmx.de>
2025-03-08 20:23:45 +0100

    enable javascript

--- src/pages/Delete-Account-Page.jsx
+++ src/pages/Delete-Account-Page.jsx
@@ -1,19 +1,21 @@
 function DeleteAccountPage() {
-    return (
-      <div className="container mt-5 pt-4">
-        <h2>Delete Your Account</h2>
-        <p>
-          If you wish to delete your account and remove all associated user data,
-          please contact us at <strong>plantfriends2024@gmail.com</strong>. Once we receive your request,
-          we will process the deletion of your account and all personal data.
-        </p>
-        <p>
-          Please note that this action is irreversible. After your account has been deleted,
-          all your data will be permanently removed from our systems.
-        </p>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="container mt-5 pt-4">
+      <h2>Delete Your Account</h2>
+      <p>
+        If you wish to delete your account and remove all associated user data,
+        please contact us at <strong>plantfriends2024@gmail.com</strong>. Once
+        we receive your request, we will process the deletion of your account
+        and all personal data. There is also an option to delete your account
+        within the application.
+      </p>
+      <p>
+        Please note that this action is irreversible. After your account has
+        been deleted, all your data will be permanently removed from our
+        systems.
+      </p>
+    </div>
+  );
+}
 
 export default DeleteAccountPage;
